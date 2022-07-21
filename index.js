bca3758f2cac9e65f51d3eae2d770e18ae274026
ivaniuss <ivan.emi94@gmail.com>
2022-07-21 01:49:30 -0500

    send tx added: :

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-//using web3js
-
 const Web3 = require('web3');
+var Accounts = require('web3-eth-accounts');
 var Contract = require('web3-eth-contract');
 const contract = require("./abi.json");
 require('dotenv').config();
-// const pubkey =`0x65AE85Dfa2b6be89F0e31E171A99b2F5981B4d58`
+const pubkey =`0x65AE85Dfa2b6be89F0e31E171A99b2F5981B4d58`
 
-Contract.setProvider(process.env.JSON_RPC);
-const myContract = new Contract(contract.abi, process.env.CONTRACT_ADDRESS);
-
+const web3 = new Web3(process.env.JSON_RPC);
+const myContract = new web3.eth.Contract(contract.abi, process.env.CONTRACT_ADDRESS);
+web3.eth.accounts.wallet.add(`0x${process.env.PRIVATE_KEY}`);
 async function main(){
-    const tx = await myContract.methods.getTokenInfo(2).call();
-    console.log('tx', tx);
+
+    var requiredGas = await myContract.methods.createToken("testweb3js","TST", "web3js.com").estimateGas({from: pubkey});
+    console.log(requiredGas);
+    
+    var rawTransaction = {
+        from: pubkey,
+        gas: 200000,
+    };
+
+
+    console.log('loading')
+    const tx = await myContract.methods.
+        createToken("testweb3js","TST", "web3js.com").
+        send(rawTransaction);
+    console.log('done')
+    console.log('tx',tx)
 }
 main()
